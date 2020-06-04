87191e61fbf15ebdcd1895e9124c0668882338d6
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-03 17:00:51 -0700

    finished post model and REST

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associations - user can make many posts 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+}); 
+
+// A post can only belong to one user 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 module.exports = { User, Post };
