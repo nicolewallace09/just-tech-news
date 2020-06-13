0779a226c0573c0b4b501a4416fb5a0d3323813e
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-12 21:28:31 -0700

    correcting login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -78,7 +78,7 @@ router.post('/', withAuth, (req, res) => {
 });
 
 // POST to identify users 
-router.post('/login', withAuth, (req, res) => {
+router.post('/login', (req, res) => {
     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
         where: {
